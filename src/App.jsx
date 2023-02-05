776d54540cba7df7e7f79f66729d873402a45245
sakusaku000 <harry.spncr@gmail.com>
2023-02-05 23:18:46 +0000

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import CardContainer from "./components/CardContainer"
+import './App.css';
+
+function App() {
+    return (
+        <main className="app-container">
+            <h1>glowy card test</h1>
+
+            <CardContainer/>
+        </main>
+    )
+}
+
+export default App
---
+++ src/components/CardContainer.jsx
@@ -0,0 +1,17 @@
+import './CardContainer.css';
+import Card from './Card';
+
+function CardContainer() {
+    return (
+        <div className="container">
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+        </div>
+    )
+}
+
+export default CardContainer
